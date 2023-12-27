b4bdf99a340145c11f96a83cdb92ac5ee2526e3d
Meghna Das <gdyes128@gmail.com>
2023-12-27 10:50:45 +0530

    changes made

--- Frontend/Reservation Page/script.js
+++ Frontend/Reservation Page/script.js
@@ -1,8 +1,4 @@
-// script.js
-
-// Function to handle table reservation
 function reserveTable() {
-    // Get the selected values
     var date = document.getElementById('date').value;
     var time = document.getElementById('time').value;
     var seats = document.getElementById('seats').value;
@@ -12,11 +8,9 @@ function reserveTable() {
         return;
     }
 
-    // Create a new row for the reservation table
     var table = document.getElementById('reservationTable').getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.rows.length);
 
-    // Insert cells with reservation information
     var cell1 = newRow.insertCell(0);
     var cell2 = newRow.insertCell(1);
     var cell3 = newRow.insertCell(2);
@@ -26,7 +20,6 @@ function reserveTable() {
     cell2.innerHTML = time;
     cell3.innerHTML = seats;
 
-    // Add cancel reservation button
     var cancelBtn = document.createElement("button");
     cancelBtn.innerHTML = "Cancel";
     cancelBtn.onclick = function() {
@@ -34,16 +27,12 @@ function reserveTable() {
     };
     cell4.appendChild(cancelBtn);
 }
-
-// Function to show cancellation confirmation popup
 function showCancellationConfirmation(row) {
     var confirmation = confirm("Are you sure you want to cancel this reservation?");
     if (confirmation) {
         cancelReservation(row);
     }
 }
-
-// Function to handle canceling a reservation
 function cancelReservation(row) {
     var table = document.getElementById('reservationTable').getElementsByTagName('tbody')[0];
     table.removeChild(row);
